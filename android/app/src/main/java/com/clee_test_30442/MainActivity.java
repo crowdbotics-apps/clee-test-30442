be4a5b4e08535f61af562ae78e21bf0fb8e8e951
Crowdbotics <team@crowdbotics.com>
2021-09-10 18:13:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/clee_test_30442/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clee_test_30442;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "clee_test_30442";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "clee_test_30442Identifier",
+  url: "https://clee_test_30442Identifier.botics.co",
+  api: "https://clee_test_30442Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
